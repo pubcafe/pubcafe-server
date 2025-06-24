d4850eabc3ef1974fb2dd1a7c84ba32368230cd2
Lee ChanMi <anytime0224@gmail.com>
2025-06-25 02:37:40 +0900

    :sparkles: feat: 추가 기능 관련 어노테이션 추가 (jpa auditing, configuration properties 등)

--- api/src/main/kotlin/com/pubcafe/api/ApiServerApplication.kt
+++ api/src/main/kotlin/com/pubcafe/api/ApiServerApplication.kt
@@ -1,9 +1,15 @@
 package com.pubcafe.api
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.domain.EntityScan
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
+@EnableJpaAuditing
+@ConfigurationPropertiesScan
+@EntityScan(basePackages = ["com.pubcafe.core.entity"])
 class ServerApplication
 
 fun main(args: Array<String>) {
