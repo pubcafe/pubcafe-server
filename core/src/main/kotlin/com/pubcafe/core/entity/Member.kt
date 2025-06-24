b3fd5c281b96148b749395a60b155392e62e02da
Lee ChanMi <anytime0224@gmail.com>
2025-06-25 02:20:41 +0900

    :sparkles: feat: 간략한 Member 엔티티 정의

---
+++ core/src/main/kotlin/com/pubcafe/core/entity/Member.kt
@@ -0,0 +1,33 @@
+package com.pubcafe.core.entity
+
+import com.pubcafe.core.entity.common.BaseTimeEntity
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "member")
+class Member(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+
+    @Column(name = "name")
+    var name: String? = null,
+
+    @Column(name = "email", nullable = false, unique = true)
+    var email: String,
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role")
+    var role: MemberRole
+
+) : BaseTimeEntity() {
+
+    companion object {
+        fun createNew(email: String): Member {
+            return Member(
+                email = email,
+                role = MemberRole.NEW_MEMBER
+            )
+        }
+    }
+}
