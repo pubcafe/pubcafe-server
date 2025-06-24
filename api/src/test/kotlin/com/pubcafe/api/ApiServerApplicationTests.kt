a8610ff81ad403f2797fc83a939bceae65e583bc
Lee ChanMi <anytime0224@gmail.com>
2025-06-25 02:38:25 +0900

    :sparkles: fix: 의존성 수정 후 자동생성 테스트 수정

--- api/src/test/kotlin/com/pubcafe/api/ApiServerApplicationTests.kt
+++ api/src/test/kotlin/com/pubcafe/api/ApiServerApplicationTests.kt
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Import
 
 @Import(TestcontainersConfiguration::class)
 @SpringBootTest
-class ServerApplicationTests {
+class ApiServerApplicationTests {
 
 	@Test
 	fun contextLoads() {
