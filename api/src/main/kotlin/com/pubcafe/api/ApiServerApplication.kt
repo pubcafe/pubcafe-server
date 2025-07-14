23f4c8caca97f4c1919bb591a1217b2ad8c4a35c
Lee ChanMi <anytime0224@gmail.com>
2025-07-15 00:21:17 +0900

    :goal_net: feat: 권한이 없을 때 반환하는 security error handler 정의

--- api/src/main/kotlin/com/pubcafe/api/ApiServerApplication.kt
+++ api/src/main/kotlin/com/pubcafe/api/ApiServerApplication.kt
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableMethodSecurity
 @ConfigurationPropertiesScan
 @EntityScan(basePackages = ["com.pubcafe.core.domain"])
 class ServerApplication
--- api/src/main/kotlin/com/pubcafe/api/common/exception/GlobalExceptionHandler.kt
+++ api/src/main/kotlin/com/pubcafe/api/common/exception/GlobalExceptionHandler.kt
@@ -4,37 +4,56 @@ import com.pubcafe.api.common.model.ResponseCode
 import com.pubcafe.api.common.model.ResponseForm
 import com.pubcafe.api.common.util.logger
 import org.springframework.http.ResponseEntity
+import org.springframework.security.authorization.AuthorizationDeniedException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 
+//TODO: 왜 에러 응답 안잡히는지 알아보기 -> CustomAccessDeniedHandler 구현하기
 @RestControllerAdvice
 class GlobalExceptionHandler {
 
     private val log = logger()
 
-    @ExceptionHandler(BusinessException::class)
-    fun handleBusinessException(ex: BusinessException): ResponseEntity<ResponseForm<Unit>> {
-        log.warn("Business exception: {} {}", ex.javaClass.simpleName, ex.getFullMessage())
+    @ExceptionHandler(AuthorizationDeniedException::class)
+    fun handleAuthorizationDeniedException(e: AuthorizationDeniedException): ResponseEntity<ResponseForm<Unit>> {
+        log.warn("Security exception: {} {}", e.javaClass.simpleName, e.message, e)
 
-        val response = ResponseForm<Unit>(
-            code = ex.code.name,
-            message = ex.getFullMessage()
-        )
-
-        return ResponseEntity.status(ex.code.httpStatus).body(response)
-    }
-
-    @ExceptionHandler(Exception::class)
-    fun handleUnknownException(ex: Exception): ResponseEntity<ResponseForm<Unit>> {
-        log.error("Unhandled exception occurred: {} {}", ex.javaClass.simpleName, ex.message)
-        val code: ResponseCode = ResponseCode.INTERNAL_SERVER_ERROR
-
-        // TODO: 프로덕트 환경에서 에러 메시지 수정
-        val response = ResponseForm<Unit>(
-            code = code.name,
-            message = "${code.message} ${ex.message ?: ""}".trim()
+        val code = ResponseCode.FORBIDDEN_ERROR;
+        val response = ResponseForm.fail(
+            code = code,
+            message = formatMessage(code.message, e.message)
         )
 
         return ResponseEntity.status(code.httpStatus).body(response)
+    }
+
+    @ExceptionHandler(BusinessException::class)
+    fun handleBusinessException(e: BusinessException): ResponseEntity<ResponseForm<Unit>> {
+        log.warn("Business exception: {} {}", e.javaClass.simpleName, e.getFullMessage(), e)
+
+        val response = ResponseForm.fail(
+            code = e.code,
+            message = e.getFullMessage()
+        )
+
+        return ResponseEntity.status(e.code.httpStatus).body(response)
+    }
+
+    @ExceptionHandler(Exception::class)
+    fun handleUnknownException(e: Exception): ResponseEntity<ResponseForm<Unit>> {
+        log.error("Unhandled exception occurred: {} {}", e.javaClass.simpleName, e.message, e)
+
+        // TODO: 프로덕트 환경에서 에러 메시지 수정
+        val code: ResponseCode = ResponseCode.INTERNAL_SERVER_ERROR
+        val response = ResponseForm.fail(
+            code = code,
+            message = this.formatMessage(code.message, e.message)
+        )
+
+        return ResponseEntity.status(code.httpStatus).body(response)
+    }
+
+    private fun formatMessage(message: String, detail: String?): String {
+        return "$message ${detail ?: ""}".trim()
     }
 }
