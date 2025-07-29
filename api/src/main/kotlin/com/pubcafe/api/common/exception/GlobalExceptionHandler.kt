04dc8c633445b46771063019d8c241adda078e2f
Lee ChanMi <anytime0224@gmail.com>
2025-07-29 22:42:23 +0900

    :goal_net: fix: 요청 값의 type이 일치하지 않는 에러 핸들링

--- api/src/main/kotlin/com/pubcafe/api/common/exception/GlobalExceptionHandler.kt
+++ api/src/main/kotlin/com/pubcafe/api/common/exception/GlobalExceptionHandler.kt
@@ -7,16 +7,31 @@ import org.springframework.http.ResponseEntity
 import org.springframework.security.authorization.AuthorizationDeniedException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException
 
-//TODO: 왜 에러 응답 안잡히는지 알아보기 -> CustomAccessDeniedHandler 구현하기
 @RestControllerAdvice
 class GlobalExceptionHandler {
 
     private val log = logger()
 
+    // 요청 값의 타입이 일치하지 않을 때
+    @ExceptionHandler(MethodArgumentTypeMismatchException::class)
+    fun handleMethodArgumentTypeMismatchException(e: MethodArgumentTypeMismatchException): ResponseEntity<ResponseForm<Unit>> {
+        log.warn("BAD REQUEST exception: {} {}", e.javaClass.simpleName, e.message, e)
+
+        val code = ResponseCode.BAD_REQUEST_ERROR;
+        val response = ResponseForm.fail(
+            code = code,
+            message = formatMessage(code.message, e.message)
+        )
+
+        return ResponseEntity.status(code.httpStatus).body(response)
+    }
+
+    //TODO: 왜 에러 응답 안잡히는지 알아보기 -> CustomAccessDeniedHandler 구현하기
     @ExceptionHandler(AuthorizationDeniedException::class)
     fun handleAuthorizationDeniedException(e: AuthorizationDeniedException): ResponseEntity<ResponseForm<Unit>> {
-        log.warn("Security exception: {} {}", e.javaClass.simpleName, e.message, e)
+        log.warn("SECURITY exception: {} {}", e.javaClass.simpleName, e.message, e)
 
         val code = ResponseCode.FORBIDDEN_ERROR;
         val response = ResponseForm.fail(
@@ -29,7 +44,7 @@ class GlobalExceptionHandler {
 
     @ExceptionHandler(BusinessException::class)
     fun handleBusinessException(e: BusinessException): ResponseEntity<ResponseForm<Unit>> {
-        log.warn("Business exception: {} {}", e.javaClass.simpleName, e.getFullMessage(), e)
+        log.warn("BUSINESS exception: {} {}", e.javaClass.simpleName, e.getFullMessage(), e)
 
         val response = ResponseForm.fail(
             code = e.code,
@@ -41,7 +56,7 @@ class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception::class)
     fun handleUnknownException(e: Exception): ResponseEntity<ResponseForm<Unit>> {
-        log.error("Unhandled exception occurred: {} {}", e.javaClass.simpleName, e.message, e)
+        log.error("SERVER Unhandled exception occurred: {} {}", e.javaClass.simpleName, e.message, e)
 
         // TODO: 프로덕트 환경에서 에러 메시지 수정
         val code: ResponseCode = ResponseCode.INTERNAL_SERVER_ERROR
